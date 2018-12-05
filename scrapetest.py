16a09e32984760d3698be9a83b049a7b480f2ecc
zyq <1810272033@email.szu.edu.cn>
2018-12-05 18:10:19 +0800

    第一章代码

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,21 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.html.body.h1)
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.html.body.h1
+    except AttributeError as e:
+        return title
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
