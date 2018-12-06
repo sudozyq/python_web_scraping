093ce7d4d6c747875e83c97324f70f3f5a413b90
zyq <1810272033@email.szu.edu.cn>
2018-12-06 14:41:41 +0800

    -m第二章dom操作代码

--- chapter2.py
+++ chapter2.py
@@ -1,11 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://www.pythonscraping.com/pages/warandpeace.html")
+html = urlopen("https://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, features="html.parser")
 # nameList = bsObj.find_all("span", {"class":{"green","red"}})
-allText = bsObj.findAll(id = "text")
-print(allText[0].get_text())
+# allText = bsObj.findAll(id = "text")
+# print(allText[0].get_text())
 
 # nameList = bsObj.find_all(text="the prince")
 # print(len(nameList))
@@ -13,3 +13,13 @@ print(allText[0].get_text())
 # for name in nameList:
 #     print(name)
 #     print(name.get_text())
+# 处理子标签
+# for child in bsObj.find("table", {"id":"giftList"}).children:
+#     print(child)
+
+# 处理兄弟标签
+# for sibling in bsObj.find("table",{"id":"giftList"}).tr.next_silblings:
+#     print(sibling)
+
+# 父标签处理
+print(bsObj.find("img", {"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
